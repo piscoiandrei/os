a03a043d4f7e888e53e5f460351503b3b50c2f9c
Piscoi Andrei <68329259+piscoiandrei@users.noreply.github.com>
2023-05-26 14:35:45 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,24 +1,12 @@
-#!/bin/bash
+output_gcc=$(gcc -Wall "$1" 2>&1)
+exit_code=$?
 
-# Check if the user passed in a file name
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <filename>"
-  exit 1
+if test $exit_code -ne 0
+then
+    errors=$(echo "$output_gcc" | grep -c "error" )
+    warnings=$(echo "$output_gcc" | grep -c "warning" )
+
+    echo "$errors/$warnings"
+else
+    echo "OK"
 fi
-
-# Check if the file exists
-if [ ! -f "$1" ]; then
-  echo "File not found: $1"
-  exit 1
-fi
-
-# Compile the file and count the errors and warnings
-output=$(gcc -o /dev/null -Wall -Wextra -pedantic-errors "$1" 2>&1)
-errors=$(echo "$output" | grep -c "error:")
-warnings=$(echo "$output" | grep -c "warning:")
-
-# Print the results
-echo "$errors $warnings"
-
-# Exit successfully
-exit 0
